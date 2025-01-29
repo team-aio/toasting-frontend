e89a1a6718f5103dae8d029cea4fb431539d01e1
YOON-CC <whdbscks77@gmail.com>
2025-01-29 13:33:16 +0900

    feat: 메시지 레이아웃 적용

--- src/components/layout/FloatingMenu.tsx
+++ src/components/layout/FloatingMenu.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Input from "../ui/Input";
+import Image from "next/image";
 
 export default function FloatingMenu() {
   const [open, setOpen] = useState(false);
@@ -71,25 +72,39 @@ export default function FloatingMenu() {
             placeholder="메시지를 검색해보세요"
           />
           {/* 채팅 리스트 */}
-          <div className="mt-4 space-y-3 h-[450px] overflow-auto">
+          <div className="mt-4 space-y-3 h-[450px] w-[100%] overflow-auto">
             {chatList.map((chat, index) => (
               <div
                 key={index}
-                className="flex justify-between items-center p-3 border-b"
+                className="flex justify-between items-center py-3 border-b w-full"
               >
-                <div>
-                  <div className="font-bold">{chat.name}</div>
+                <Image
+                  src={"/layout/profile.svg"}
+                  width={70}
+                  height={70}
+                  alt="프로필"
+                />
+                <div className="w-[100%] items-center ml-4">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <div className="font-bold items-center text-black">
+                        {chat.name}
+                      </div>
+                      <div className="text-sm text-gray-400 ml-2">
+                        {chat.time}
+                      </div>
+                    </div>
+
+                    {chat.messageCount > 0 && (
+                      <div className="bg-black text-white text-xs rounded-full w-5 h-5 flex items-center justify-center ">
+                        {chat.messageCount}
+                      </div>
+                    )}
+                  </div>
+
                   <div className="text-sm text-gray-500">
                     {chat.lastMessage}
                   </div>
-                </div>
-                <div className="text-right">
-                  <div className="text-sm text-gray-400">{chat.time}</div>
-                  {chat.messageCount > 0 && (
-                    <div className="bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center mt-1">
-                      {chat.messageCount}
-                    </div>
-                  )}
                 </div>
               </div>
             ))}
