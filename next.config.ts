44a26f3ce68172a167a0af8e253f7b77c7cb098b
YOON-CC <whdbscks77@gmail.com>
2025-03-24 23:03:49 +0900

    환경설정파일 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_REACT_APP_API_URL: process.env.NEXT_PUBLIC_REACT_APP_API_URL,
+    NEXT_PUBLIC_AUTH_SECRET: process.env.NEXT_PUBLIC_AUTH_SECRET,
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET:
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  },
 };
 
 export default nextConfig;
