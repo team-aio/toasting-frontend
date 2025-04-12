387e459d3f4c773cc7e6c0ba951cd5f99c7edae0
YOON-CC <whdbscks77@gmail.com>
2025-04-13 00:00:55 +0900

    feat: 미들웨어 적용

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,28 @@
-// import { auth } from "@/auth";
-// import { NextResponse } from "next/server";
-// import { cookies } from "next/headers";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function middleware() {
-  // const session = await auth();
-  // const authorization = (await cookies()).get("authorization")?.value;
-  // const uuid = (await cookies()).get("uuid")?.value;
-  // // 엑세스 토큰이랑, uuid가 널일경우
-  // if (authorization === null || uuid === null) {
-  //   return NextResponse.redirect("https://fe-meetplus.vercel.app/login");
-  // }
-  // // 로그인을 안했을때 만 해당
-  // if (!session) {
-  //   return NextResponse.redirect("https://fe-meetplus.vercel.app/login");
-  // }
+  const session = await auth();
+  const authorization = (await cookies()).get("authorization")?.value;
+  const memberId = (await cookies()).get("memberId")?.value;
+
+  // console.log(
+  //   "미들웨어에서 부른 authorization, memberId",
+  //   authorization,
+  //   memberId
+  // );
+  // 엑세스 토큰이랑, uuid가 널일경우
+  if (authorization === undefined || memberId === undefined) {
+    return NextResponse.redirect("https://www.toasting.io/");
+  }
+  // 로그인을 안했을때 만 해당
+  if (!session) {
+    return NextResponse.redirect("https://www.toasting.io/");
+  }
 }
 
 export const config = {
   // 적용이 되는 곳 선정
-  matcher: [],
+  matcher: ["/profile/:path*", "/search/:path*"],
 };
